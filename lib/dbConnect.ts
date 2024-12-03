bd21c2d3de783a3d9aafa98a7dc704fe39db7ea0
Mustafa Ghouri <112586426+MustafaGhouri@users.noreply.github.com>
2024-12-04 03:12:31 +0500

    changes

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 async function dbConnect() {
   try {
+    if (mongoose.connection.readyState >= 1) return;
+    
     await mongoose.connect(process.env.MONGODB_URI!);
   } catch (error) {
     throw new Error('Connection failed!');
